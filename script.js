87bdd090291e667db75d14c99090f8e4c9136583
Even <even.russom@strathmore.edu>
2024-01-20 16:49:40 +0300

    Added code to fetch and display current weather

--- script.js
+++ script.js
@@ -8,4 +8,24 @@ function getWeather(){
     }
     const currentWeartherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
     const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+
+    fetch(currentWeartherUrl)
+    .then(response => response.json())
+    .then(data => {
+        displayWeather(data);
+    })
+    .catch(error =>{
+        console.error('Error fetching current weather data:', error);
+        alert('Error fetching current weather data. Please try again.');
+    });
+
+    fetch(forecastUrl)
+    .then(response => response.json())
+    .then(data => {
+        displayHourlyForecast(data.list);
+    })
+    .catch(error =>{
+        console.error('Error fetching hourly forecast data:', error);
+        alert('Error fetching current weather data. Please try again.');
+    });
 }   
