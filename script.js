f28e175b7ced09c7f235e5d29c89a542690f81fc
Even <even.russom@strathmore.edu>
2024-01-20 10:14:15 +0300

    Added alerts for empty city input

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function getWeather(){
     const apiKey = '541ff514fce04ac02977528535484cd6';
     const city = document.getElementById('city').value;
-    
-}
+
+    if (!city){
+        alert('Please enter city');
+        return;
+    }
+    const currentWeartherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+}   
